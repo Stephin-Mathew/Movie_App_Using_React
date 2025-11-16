f57f8596f55c54a634ba899c545aaf3910e8612e
Your Name <email@something.com>
2025-11-16 14:17:12 +0530

    appwrite setting up

---
+++ src/appwrite.js
@@ -0,0 +1,8 @@
+const DATABASE_ID=import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const PROJECT_ID=import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const COLLECTION_ID=import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+
+export const updateSearchCount = async () =>  {
+    console.log(DATABASE_ID,PROJECT_ID,COLLECTION_ID);
+}
